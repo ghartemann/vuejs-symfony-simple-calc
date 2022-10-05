10d31652c1577cb4a451cbf2bf47c2f506bdd732
Guillaume HARTEMANN-PIOLLET <ghartemann@com-company.fr>
2022-10-05 11:45:36 +0200

    working bootstrapped calc

--- assets/js/App.vue
+++ assets/js/App.vue
@@ -2,25 +2,70 @@
 export default {
   data() {
     return {
-      name: '',
-      hello: ''
+      number1: 2,
+      number2: 3,
+      operator: '+',
     }
   },
   methods: {
-    refreshHello() {
-      if (this.name) {
-        fetch("/api/helloword/" + this.name, {"method": "GET"})
-            .then(response => response.json())
-            .then(result => this.hello = result);
+    changeSymbol(operator) {
+      switch (operator) {
+        case "+":
+          this.operator = "*";
+          break;
+        case "*":
+          this.operator = "-";
+          break;
+        case "-":
+          this.operator = "/";
+          break;
+        case "/":
+          this.operator = "+";
+          break;
       }
     }
+  },
+  computed: {
+    resultAddition() {
+      return this.number1 + this.number2;
+    },
+    resultMultiplication() {
+      return this.number1 * this.number2;
+    },
+    resultSoustraction() {
+      return this.number1 - this.number2;
+    },
+    resultDivision() {
+      return this.number1 / this.number2;
+    }
   }
-}
+};
 </script>
 
 <template>
-  <h1>toto</h1>
-  <input type="text" v-model="name">
-  <button @click="refreshHello">Demander un bonjour !</button>
-  <h1 v-show="!!hello">{{ hello }}</h1>
+  <div class="row justify-content-center mt-5">
+    <div class="col-6 card shadow p-3">
+      <h1 class="mb-4">Simple calc</h1>
+      <form>
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" id="input1" placeholder="Enter number 1" v-model.number="number1">
+          <span class="input-group-text user-select-none" id="operator"
+                @click="changeSymbol(operator)">{{ operator }}</span>
+          <input type="text" class="form-control" id="input2" placeholder="Enter number 2" v-model.number="number2">
+          <span class="input-group-text user-select-none" id="equals">=</span>
+
+          <!-- horrendous block of code -->
+          <input type="text" class="form-control bg-white" id="result" placeholder="Result" v-if="operator === '+'"
+                 v-model.number="resultAddition" disabled readonly>
+          <input type="text" class="form-control bg-white" id="result" placeholder="Result" v-if="operator === '*'"
+                 v-model.number="resultMultiplication" disabled readonly>
+          <input type="text" class="form-control bg-white" id="result" placeholder="Result" v-if="operator === '-'"
+                 v-model.number="resultSoustraction" disabled readonly>
+          <input type="text" class="form-control bg-white" id="result" placeholder="Result" v-if="operator === '/'"
+                 v-model.number="resultDivision" disabled readonly>
+        </div>
+      </form>
+
+    </div>
+  </div>
 </template>
--- webpack.config.js
+++ webpack.config.js
@@ -57,23 +57,24 @@ Encore
     })
 
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     .enableTypeScriptLoader()
-    .enableVueLoader(() => {}, { runtimeCompilerBuild: false })
+    .enableVueLoader(() => {
+    }, {runtimeCompilerBuild: false})
 
-    // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
+// uncomment if you use TypeScript
+//.enableTypeScriptLoader()
 
-    // uncomment if you use React
-    //.enableReactPreset()
+// uncomment if you use React
+//.enableReactPreset()
 
-    // uncomment to get integrity="..." attributes on your script & link tags
-    // requires WebpackEncoreBundle 1.4 or higher
-    //.enableIntegrityHashes(Encore.isProduction())
+// uncomment to get integrity="..." attributes on your script & link tags
+// requires WebpackEncoreBundle 1.4 or higher
+//.enableIntegrityHashes(Encore.isProduction())
 
-    // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+// uncomment if you're having problems with a jQuery plugin
+//.autoProvidejQuery()
 ;
 
 module.exports = Encore.getWebpackConfig();
